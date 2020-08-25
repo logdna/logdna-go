9a1fd311b3fded275a591f6b666d35fa4fe32413
Mike Del Tito <mike.deltito@logdna.com>
2020-08-25 11:18:23 -0400

    refactor!: change meta format, add comprehensive tests
    BREAKING CHANGE:
    * rework tests to not rely on external services
    * add test run and coveralls to CI
    * refactor options and allow for message-level overrides via `merge()`
    * removed `LogWithLevelAndApp()` - `LogWithOptions()` should be used
      instead
    * change meta format
      * Meta is now expected to be supplied as a valid JSON string
      * Meta is now properly marshalled to either a string or an object
        in the line payload depending on the IndexMeta option
    
    Semver: major
    Ref: LOG-6966,LOG-7026

---
+++ logger/options.go
@@ -0,0 +1,104 @@
+package logger
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strings"
+	"time"
+)
+
+var defaultIngestURL = "https://logs.logdna.com/logs/ingest"
+
+// InvalidOptionMessage represents an issue with the supplied configuration.
+type InvalidOptionMessage struct {
+	Option  string
+	Message string
+}
+
+// Options encapsulates user-provided options such as the Level and App
+// that are passed along with each log.
+type Options struct {
+	App           string
+	Env           string
+	FlushInterval time.Duration
+	Hostname      string
+	IndexMeta     bool
+	IngestURL     string
+	IPAddress     string
+	Level         string
+	MacAddress    string
+	MaxBufferLen  int
+	Meta          string
+	Tags          string
+}
+
+func (e InvalidOptionMessage) String() string {
+	return fmt.Sprintf("Options.%s: %s", e.Option, e.Message)
+}
+
+func validateOptionLength(option string, value string, problems *[]string) {
+	if len(value) > 32 {
+		*problems = append(*problems, InvalidOptionMessage{option, "length must be less than 32"}.String())
+	}
+}
+
+func (options *Options) validate() error {
+	var problems []string
+	reMacAddress := regexp.MustCompile(`^([0-9a-fA-F][0-9a-fA-F]:){5}([0-9a-fA-F][0-9a-fA-F])`)
+	reHostname := regexp.MustCompile(`(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9-]*[A-Za-z0-9])`)
+	reIPAddress := regexp.MustCompile(`(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}`)
+
+	validateOptionLength("App", options.App, &problems)
+	validateOptionLength("Env", options.Env, &problems)
+	validateOptionLength("Hostname", options.Hostname, &problems)
+	validateOptionLength("Level", options.Level, &problems)
+
+	if options.MacAddress != "" && (!reMacAddress.MatchString(options.MacAddress)) {
+		problems = append(problems, InvalidOptionMessage{"MacAddress", "invalid format"}.String())
+	}
+
+	if options.Hostname != "" && !reHostname.MatchString(options.Hostname) {
+		problems = append(problems, InvalidOptionMessage{"Hostname", "invalid format"}.String())
+	}
+
+	if options.IPAddress != "" && !reIPAddress.MatchString(options.IPAddress) {
+		problems = append(problems, InvalidOptionMessage{"IPAddress", "invalid format"}.String())
+	}
+
+	if len(problems) > 0 {
+		return errors.New(strings.Join(problems, ", "))
+	}
+
+	return nil
+}
+
+func (options Options) merge(merge Options) Options {
+	newOpts := options
+	if merge.App != "" {
+		newOpts.App = merge.App
+	}
+	if merge.Env != "" {
+		newOpts.Env = merge.Env
+	}
+	if merge.Level != "" {
+		newOpts.Level = merge.Level
+	}
+	if merge.Meta != "" {
+		newOpts.Meta = merge.Meta
+	}
+
+	return newOpts
+}
+
+func (options *Options) setDefaults() {
+	if options.FlushInterval == 0 {
+		options.FlushInterval = 5 * time.Second
+	}
+	if options.MaxBufferLen == 0 {
+		options.MaxBufferLen = 5
+	}
+	if options.IngestURL == "" {
+		options.IngestURL = defaultIngestURL
+	}
+}
